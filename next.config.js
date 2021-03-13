96bee506063ca063f3576922b8fccffc76e68854
ManosEarlyNode <fregkos.emmanouil@earlynode.com>
2021-03-13 20:02:19 +0200

    change db 2

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = (phase) => {
+  return {
+    env: {
+      mongodb_username: "ManosFr",
+      mongodb_password: "6944238811e",
+      mongodb_cluster: "cluster0",
+      mongodb_database: "events",
+    },
+  };
+};
